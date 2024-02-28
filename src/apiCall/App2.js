2b9a006ba3a6e398ee6f47b926fef44f7c85293c
Naman Jindal <namanjindal113@gmail.com>
2024-02-28 22:15:59 +0530

    post api

--- src/apiCall/App2.js
+++ src/apiCall/App2.js
@@ -23,25 +23,40 @@ export default function App2() {
         }, 3000)
         
     }, [])
-  return (
+    async function callPostAPI(){
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'Post',
+            body: JSON.stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+              }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+              },
+        })
+        console.log(await response.json())
+    }
+    function sendPost(){
+        callPostAPI();
+    }
+    if(isLoading){
+        return "loading"
+    }
+  return ( 
     <>
-
-        {/* {isLoading && "loading"} */}
-        {isLoading ? "laoading" :
-            <>
-                {commentData.map((comment)=>{
-                    return (
-                        <div className='card' key={comment.id} style={{border: '1px solid black', width: '300px'}}>
-                            <div>{comment.body}</div>
-                            <div style={{display: 'flex'}}>
-                                <span>{comment.name}</span>
-                                <span>{comment.email}</span>
-                            </div>
-                        </div>
-                    )
-                })}
-            </>
-        }
+        {commentData.map((comment)=>{
+            return (
+                <div className='card' key={comment.id} style={{border: '1px solid black', width: '300px'}}>
+                    <div>{comment.body}</div>
+                    <div style={{display: 'flex'}}>
+                        <span>{comment.name}</span>
+                        <span>{comment.email}</span>
+                    </div>
+                </div>
+            )
+        })}
+        <button onClick={sendPost}>Send Post Data</button>
     </>
   )
 }
