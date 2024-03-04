7f18c76b016c6b9283d99eeb00e57c581e342566
Naman Jindal <namanjindal113@gmail.com>
2024-03-04 22:59:18 +0530

    styling almost complete of pokemon project

--- src/pokedex/components/PokemonModal.js
+++ src/pokedex/components/PokemonModal.js
@@ -2,12 +2,54 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './modal.css'
 export default function PokemonModal({pokemonData, closeModal}) {
-  console.log(pokemonData)
+  const {height, weight, name, image, type} = pokemonData;
+  const bs =[];
+  const stats=[];
+
+  pokemonData.stats.forEach(element => {
+    if(element.base_stat){
+      bs.push(element.base_stat);
+    }
+    if(element.stat && element.stat.name){
+      stats.push(element.stat.name)
+    }
+  })
+  console.log(bs);
+  console.log(stats);
   return ReactDOM.createPortal(
     <div className='modal'>
-      <div className='content'>
-        abcd
-        <button onClick={closeModal}>close modal</button>
+      <div className={`content flex ${type}`}>
+        <div className='pokemonDisplay flex'>
+          <div className='pokemonImageContainer'>
+            <img src={image} />
+          </div>
+          <div className='pokemonName'>
+            {name}
+          </div>
+        </div>
+        <div className={`pokemonStats flex ${type}`}>
+          <div className='flexColumn'>
+            <div>weight: {weight}</div>
+            <div>height: {height}</div>
+          </div>
+          
+          <div className='flexColumn' style={{rowGap: '17px', width: '124px'}}>
+            {stats.map((stat, index)=>{
+                return (
+                  <div key={index}>stat{index+1}: {stat}</div>
+                )
+            })}
+          </div>
+          <div className='flexColumn'>
+            {bs.map((baseStat, index)=>{
+              return (
+                <div key={index}>Bs{index+1}: {baseStat}</div>
+              )
+            })}
+
+          </div>
+        </div>
+        <button onClick={closeModal} className={`${type} modalBtn`}>X</button>
       </div>
       
     </div>,
