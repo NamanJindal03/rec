ab8fa7b3c06d4452e0f305b50968c9011c4ab8b9
Naman Jindal <namanjindal113@gmail.com>
2024-03-11 22:54:27 +0530

    outlet

--- src/reactRouterDom/BlogLayout.js
+++ src/reactRouterDom/BlogLayout.js
@@ -1,19 +1,30 @@
 import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, NavLink } from 'react-router-dom'
+import './tempStyle.css'
 
 export default function BlogLayout() {
   return (
     <>
         <nav>
-          <ul style={{display: 'flex', gap:'10px', listStyle: "none"}}>
-            <li>
-              <Link to="/blogs/1">Blog 1</Link>
+          <ul style={{display: 'flex', gap:'10px', listStyle: "none"}} >
+            <li >
+              <NavLink 
+                to="/blogs/1"
+                // className={({isActive})=> isActive ? 'isActive' : '' }
+              >Blog 1</NavLink>
             </li>
             <li>
-              <Link to="/blogs/2">Blog 2</Link>
+              <NavLink 
+                to="/blogs/2"
+                // className={(state)=> state.isActive ? 'isActive' : '' }
+              >Blog 2</NavLink>
             </li>
             <li>
-              <Link to="/blogs/3">Blog 3</Link>
+              <NavLink 
+                to="/blogs/3"
+                // className={({isActive})=> isActive ? 'isActive' : '' }
+                // className={'isActive sample power'}
+              >Blog 3</NavLink>
             </li>
           </ul>
         </nav>
@@ -25,7 +36,7 @@ export default function BlogLayout() {
         <footer className='blogFooter'>
             this is my temp foot
         </footer> */}
-        <Outlet />
+        <Outlet context={{commonInfo: 'I am common'}}/>
     </>
   )
 }
--- src/reactRouterDom/pages/BlogsHome.js
+++ src/reactRouterDom/pages/BlogsHome.js
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useOutletContext } from 'react-router-dom'
 export default function BlogsHome() {
+  const data = useOutletContext();
+  console.log(data);
   return (
     <div>Temp</div>
   )
