149c16f11bc3edca507c534623568737b3749c10
Naman Jindal <namanjindal113@gmail.com>
2024-03-19 21:57:58 +0530

    counter through user reducer

--- src/useReducer/Counter.js
+++ src/useReducer/Counter.js
@@ -1,16 +1,28 @@
 import React, {useReducer} from 'react'
 
-function reducer(state){
-    console.log(state);
-    return state +1;
+function reducer(state, action){
+    switch(action.type){
+        case 'increment':
+            return state + action.payload
+        case 'decrement':
+            return state - action.payload
+        case 'reset':
+            return 0
+        default: 
+            return state
+    }
+    // console.log(state);
+    // return state+ payload;
 }
 
 export default function Counter() {
     const [state, dispatch] = useReducer(reducer, 0);
   return (
     <>
+        
+        <button onClick={()=>dispatch({type: 'increment', payload: 1})}>+</button>
         {state}
-        <button onClick={dispatch}>random</button>
+        <button onClick={()=>dispatch({type: 'decrement', payload: 1})}>-</button>
     </>
   )
 }
