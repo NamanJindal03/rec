83b08a34787be40a31358bd87e0a8f48590f8df9
Naman Jindal <namanjindal113@gmail.com>
2024-03-20 22:35:12 +0530

    consumer old syntadx

--- src/contextAPI/App.js
+++ src/contextAPI/App.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Parent1 from './Parent1'
 import Parent2 from './Parent2'
 
+import MyContextStore from './context'
+
 export default function App() {
+    const [counter, setCounter] = useState(0)
   return (
     <div>
-        <Parent1 />
-        <Parent2 />
+        <MyContextStore.Provider value={{counter: counter}}>
+            <Parent1 />
+            <Parent2 />
+        </MyContextStore.Provider>
     </div>
+
   )
 }
+
+/* 
+    {counter: counter}
+
+    ES6 syntax ->
+    {counter}
+*/
