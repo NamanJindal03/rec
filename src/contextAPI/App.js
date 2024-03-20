15b562e659704dd7b966a694fa155a79f09d2743
Naman Jindal <namanjindal113@gmail.com>
2024-03-20 21:57:00 +0530

    context base

---
+++ src/contextAPI/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Parent1 from './Parent1'
+import Parent2 from './Parent2'
+
+export default function App() {
+  return (
+    <div>
+        <Parent1 />
+        <Parent2 />
+    </div>
+  )
+}
---
+++ src/contextAPI/Child2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Child2() {
+  return (
+    <div>Child2</div>
+  )
+}
