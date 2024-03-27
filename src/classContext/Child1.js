f1caf5a0ddcf41eb64404023762ed2e42fc3501c
Naman Jindal <namanjindal113@gmail.com>
2024-03-27 22:24:07 +0530

    done

--- src/classContext/Child1.js
+++ src/classContext/Child1.js
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import { ThemeContext } from './Context/ThemeContext'
  /* import the context over here */
 export default function Child1() {
    
   return (
+    <ThemeContext.Consumer>
+    {
+        (state)=>{
+            return(
+                <div>Child1</div>
+            )
+        }
+    }
+    </ThemeContext.Consumer>
+    
     /* create the consumer wrapper over here and display the theme along with that give a button to toggle the theme */
-    <div>Child1</div>
+    
   )
 }
