a574cf75a8356ae8a2105380b80ed3f08a7ae60b
Naman Jindal <namanjindal113@gmail.com>
2024-03-27 22:36:39 +0530

    theme context

--- src/classContext/Context/ThemeContext.js
+++ src/classContext/Context/ThemeContext.js
@@ -5,6 +5,15 @@ const ThemeContext =  createContext();
 class ThemeProvider extends Component{
     // define state 
     //creatre wrapper
+    render(){
+        return(
+            /* add value prop */
+            <ThemeContext.Provider >
+                {/* add children */}
+            </ThemeContext.Provider>
+        )
+    }
+    
 }
 
 export {ThemeContext, ThemeProvider}
