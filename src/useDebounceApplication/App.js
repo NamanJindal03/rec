99cf62692d42d541c1d5dbd3cc2e2ebaee495124
Naman Jindal <namanjindal113@gmail.com>
2024-04-10 22:28:50 +0530

    usedebounce applciation custom hook

--- src/useDebounceApplication/App.js
+++ src/useDebounceApplication/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useDebounce from './useDebounce'
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+    const [value, setValue] = useState('');
+    const debouncedValue = useDebounce(value, 2000);
+
+    return (
+      <>
+          <input type="text" value={value} onChange={(e)=>setValue(e.target.value)}/>
+          <h1>The debounced value would be</h1>
+          <p>{debouncedValue}</p>
+      </>
+    )
 }
--- src/useDebounceApplication/useDebounce.js
+++ src/useDebounceApplication/useDebounce.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
-export default function useDebounce() {
-  return (
-    <div>useDebounce</div>
-  )
+export default function useDebounce(val, timer) {
+    const [state, setState] = useState(val);
+    // console.log(state)
+    // console.log(val);
+    // console.log(timer);
+    useEffect(()=>{
+        const id = setTimeout(()=>{
+            setState(val)
+        }, timer)
+
+        return ()=> {
+            clearTimeout(id)
+        }
+    },[val, timer])
+    return state;
+
 }
